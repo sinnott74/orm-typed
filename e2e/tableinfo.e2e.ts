61e4bcaadff748922c17c0e6fc6f5e00adeec824
Daniel Sinnott <sinnott74@hotmail.com>
2018-07-08 04:00:05 +0100

    Ignore test

--- e2e/tableinfo.e2e.ts
+++ e2e/tableinfo.e2e.ts
@@ -1,18 +1,18 @@
 import { transaction, init } from "../dist/orm";
 import config from "./config";
-import { compareEntityMetadataWithTable } from "../dist/tableinfo";
+import { compareEntityMetadataWithTable } from "../dist/tableinfo/tableinfo";
 import BlogPost from "../../server/dist/entity/BlogPost";
 
 beforeAll(async () => {
   init(config);
 });
 
-describe("TableInfo", () => {
-  xit("can", async () => {
-    await transaction(async () => {
-      await compareEntityMetadataWithTable(BlogPost);
-    });
-  });
-});
+// describe("TableInfo", () => {
+//   it("can", async () => {
+//     await transaction(async () => {
+//       await compareEntityMetadataWithTable(BlogPost);
+//     });
+//   });
+// });
 
 // yarn build && ../node_modules/jest/bin/jest.js tableinfo.e2e.ts
